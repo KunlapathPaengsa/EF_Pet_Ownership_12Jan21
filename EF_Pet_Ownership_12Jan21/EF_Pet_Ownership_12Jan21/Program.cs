508c48135d88bc4e2d0131aec33164a00809d5b8
Cougarpatt <69376587+KunlapathPaengsa@users.noreply.github.com>
2021-01-12 23:33:43 +0700

    Add files via upload

---
+++ EF_Pet_Ownership_12Jan21/EF_Pet_Ownership_12Jan21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EF_Pet_Ownership_12Jan21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
